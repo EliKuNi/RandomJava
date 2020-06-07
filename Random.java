30a49a4bb6c8ac8a4961a942e930c6650773f55a
Elizabeth <65304506+ElizabethKuleshova@users.noreply.github.com>
2020-06-07 12:44:14 +0200

    Create Random.java

---
+++ Random.java
@@ -0,0 +1,32 @@
+public class FirstCode2020 {
+    public static void main(String[ ] args) {
+        System.out.println("Hello World! Spots on! 🐞");
+         System.out.println(" I am now learning Java :D");
+         
+        int x = 0;
+
+        while( x < 101 )
+        {
+            System.out.println(x);
+            x++;
+        }
+        System.out.println("Pound it! ✨");
+    }
+}
+
+// I will add something else later as I learn more 
+
+// Update: and some random calculations all over the place
+
+class MyClass {
+
+    static int sum(int val1, int val2, int val3) {
+        return val1 + val2 + val3;
+    }
+
+    public static void main(String[ ] args) {
+        int x = sum(6, 8, 7);
+        System.out.println(x);
+    }
+}                                  //Oh geez why 21?? Always 21 🙄
+
